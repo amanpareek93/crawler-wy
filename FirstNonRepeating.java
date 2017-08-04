17ae5fe76df4d5236f4fab71f72e45b1414820c3
amanpareek93 <amanpareek93@gmail.com>
2017-08-04 08:45:31 +0530

    Add files via upload

---
+++ FirstNonRepeating.java
@@ -0,0 +1,68 @@
+/**
+ * Created by aman on 8/3/17.
+ */
+
+
+
+import java.util.HashMap;
+ 
+class CountIndex
+{
+    int count,index;
+
+     public CountIndex(int index) {
+        this.count = 1;
+        this.index = index;
+    }
+     
+    public void incCount() {
+        this.count++;
+    }
+}
+class FirstNonRepeating
+{
+    static final int NO_OF_CHARS = 256;
+     
+    static HashMap<Character, CountIndex> hm = new HashMap<Character, CountIndex>(NO_OF_CHARS);
+
+     static void getCharCountArray(String str) 
+     {
+         for (int i = 0; i < str.length();  i++)
+         {
+             if(hm.containsKey(str.charAt(i)))
+             {
+                 hm.get(str.charAt(i)).incCount();
+             }
+              
+             else
+             {
+                 hm.put(str.charAt(i), new CountIndex(i));
+             }
+                  
+         }    
+     }
+      
+    static int firstNonRepeating(String str)
+    {
+        getCharCountArray(str);
+        int result = Integer.MAX_VALUE, i;
+      
+        for (i = 0; i < str.length();  i++)
+        {
+            if (hm.get(str.charAt(i)).count == 1 && result > hm.get(str.charAt(i)).index){
+                 result = hm.get(str.charAt(i)).index;
+            }
+               
+        }  
+       
+      return result;
+    }
+    public static void main (String[] args)
+    {
+        String str = "amanpareek";
+        int index =  firstNonRepeating(str);
+         
+        System.out.println(index == Integer.MAX_VALUE ? "Either all characters are repeating " +
+              " or string is empty" : "First non-repeating character is " +  str.charAt(index));
+    }
+}
